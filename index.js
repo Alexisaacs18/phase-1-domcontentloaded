5d4bed283528979f338babddfe801776650890eb
Alex Isaacs <alexisaacs18@gmail.com>
2023-11-01 17:04:59 -0400

    Finished Lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code goes here
+// debugger
+
+const text = document.getElementById('text')
+
+// debugger
+
+// text.textContent = 'This is really cool!'
+
+// debugger
+
+// text.textContent = 'JavaScript is so cool. It lets me add text to my page programmatically.'
+
+// const body = document.getElementById('body')
+
+// const p = document.createElement('p')
+
+// body.appendChild(p)
+
+// p.textContent = 'This is really cool!'
+
+// const button = document.createElement('button')
+
+// body.appendChild(button)
+
+// button.textContent = 'Button'
+
+// button.addEventListener('click', (e) => alert('I was clicked!'))
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("The DOM has loaded");
+    text.textContent = 'This is really cool!'
+});
